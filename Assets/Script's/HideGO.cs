adfcfe5338b5dade43035257d0e6230c742088de
Mr-Computer <sashacomputer399@gmail.com>
2018-05-03 13:35:25 +0300

    status up

---
+++ Assets/Script's/HideGO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+public class HideGO : MonoBehaviour {
+    void Update () {
+     if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.x, 1));
+            RaycastHit _hit;
+            if (Physics.Raycast(ray, out _hit, Mathf.Infinity))
+            {
+                if (_hit.transform.tag == "DestroyObject")
+                Destroy(_hit.transform.gameObject);
+            }
+        }
+    }
+}
